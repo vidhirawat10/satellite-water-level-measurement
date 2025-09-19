25bf108115a1b916e70d58aab4f87524a4d860ab
vidhirawat10 <vidhirawat54@gmail.com>
2025-09-19 18:25:35 +0530

    feat: Configure CORS for production

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: "https://dam-analyzer.vercel.app", // Your frontend URL
+  optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
 
-// Increase the limit for JSON payloads
 app.use(express.json({ limit: '50mb' }));
-
-// Also increase the limit for URL-encoded payloads
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // API Routes
